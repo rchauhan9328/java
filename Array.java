8d1768945c9b3e09118b1fc9599b79a73e8e9c35
Ravina Chauhan <rchauhan9328@gmail.com>
2026-06-06 18:44:10 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,23 @@
+//Array is leaniar ds
+//index start 0 to n-1
+//size n
+class Array{
+    public static void main(String[] args) {
+        // int[] arr={10,20,30,40,12,15};// first method // stored 
+        // //indexing 
+        // arr[0]=10;
+        // System.out.println(arr[0]+" "+arr[3]);//access
+        // //upadating element
+        // arr[3]=15;
+        // System.err.println(arr[3]);
+        // > 2nd method 
+        int[] arr=new int[4];// 4 size ka array
+        // initalization
+        arr[0]=10;
+        arr[1]=85;
+        arr[2]=89;
+        arr[3]=78;
+
+
+    }
+}
