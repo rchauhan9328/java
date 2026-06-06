9d2fa87c0a7e002532427d0442d0e2610f1bae9a
Ravina Chauhan <rchauhan9328@gmail.com>
2026-06-06 18:47:41 +0530

    Add files via upload

---
+++ For_Each.java
@@ -0,0 +1,8 @@
+class For_Each {
+    public static void main(String[] args) {
+         int[] x={51,52,89,20,78};
+        for(int y:x){
+            System.err.print((y*2)+" ");
+        }
+    }
+}
